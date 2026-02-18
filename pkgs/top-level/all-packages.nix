a521968c49932574ddb419d69f47ad85611acd9e
Guy Chronister <guychronister@outlook.com>
2026-02-18 10:14:54 -0600

    i3lock-fancy-rapid: migrate to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9505,8 +9505,6 @@ with pkgs;
 
   sway-contrib = recurseIntoAttrs (callPackages ../applications/misc/sway-contrib { });
 
-  i3lock-fancy-rapid = callPackage ../applications/window-managers/i3/lock-fancy-rapid.nix { };
-
   i3status = callPackage ../applications/window-managers/i3/status.nix { };
 
   i3wsr = callPackage ../applications/window-managers/i3/wsr.nix { };
