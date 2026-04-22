16dfcd3e0ffa89f8d7e41199e6bb22ab7f90a14b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2026-04-23 00:05:56 +0200

    python314Packages.audio-hotplug: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1224,6 +1224,8 @@ self: super: with self; {
 
   audible = callPackage ../development/python-modules/audible { };
 
+  audio-hotplug = callPackage ../development/python-modules/audio-hotplug { };
+
   audioop-lts =
     if pythonAtLeast "3.13" then callPackage ../development/python-modules/audioop-lts { } else null;
 
