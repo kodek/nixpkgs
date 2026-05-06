c9ff83a4d1016d231cf9f87b2e8923e11e8dd790
SkohTV <contact@skoh.dev>
2026-05-06 16:45:57 -0400

    python3Packages.pyfaidx: modernize

--- pkgs/development/python-modules/pyfaidx/default.nix
+++ pkgs/development/python-modules/pyfaidx/default.nix
@@ -13,7 +13,7 @@
   setuptools-scm,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pyfaidx";
   version = "0.9.0.4";
   pyproject = true;
@@ -21,7 +21,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "mdshw5";
     repo = "pyfaidx";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-hefRqpI9xzlfdUbr8mpQ6I1+EGAmS50f28avbtRMlSk=";
   };
 
@@ -51,9 +51,9 @@ buildPythonPackage rec {
   meta = {
     description = "Python classes for indexing, retrieval, and in-place modification of FASTA files using a samtools compatible index";
     homepage = "https://github.com/mdshw5/pyfaidx";
-    changelog = "https://github.com/mdshw5/pyfaidx/releases/tag/${src.tag}";
+    changelog = "https://github.com/mdshw5/pyfaidx/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ jbedo ];
     mainProgram = "faidx";
   };
-}
+})
