7fda83d316291d4fafeef9ef1d83bbec0c571741
Will Fancher <elvishjerricco@gmail.com>
2026-05-13 17:08:47 -0400

    Revert "nixos/console: Fix systemd-vconsole-setup"
    This reverts commit ed6c1b08f9ff07d6e10406650b4924874a2757ec.

--- nixos/modules/config/console.nix
+++ nixos/modules/config/console.nix
@@ -189,9 +189,6 @@ in
             "/etc/kbd/keymaps" = lib.mkIf (!cfg.earlySetup) {
               source = "${consoleEnv config.boot.initrd.systemd.package.kbd}/share/keymaps";
             };
-            "/etc/kbd/consolefonts" = lib.mkIf (!cfg.earlySetup && cfg.font != null) {
-              source = "${consoleEnv config.boot.initrd.systemd.package.kbd}/share/consolefonts";
-            };
           };
           boot.initrd.systemd.additionalUpstreamUnits = [
             "systemd-vconsole-setup.service"
