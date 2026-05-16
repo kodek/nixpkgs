5366e1400c8c8e572e296ff68adf25d5a6daad14
Angel J <78835633+iamanaws@users.noreply.github.com>
2026-05-15 20:56:39 -0700

    dblab: cleanup

--- pkgs/by-name/db/dblab/package.nix
+++ pkgs/by-name/db/dblab/package.nix
@@ -11,21 +11,30 @@ buildGoModule (finalAttrs: {
   src = fetchFromGitHub {
     owner = "danvergara";
     repo = "dblab";
-    rev = "v${finalAttrs.version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-0tkIDWAub+wfoJ760m1kU7XYnGNner/zLtCod6UPF60=";
   };
 
   vendorHash = "sha256-B5wyERNUkJIrKjKET9HX3F43CFW6aBtzAarkAuhxw9o=";
 
-  ldflags = [ "-s -w -X main.version=${finalAttrs.version}" ];
+  ldflags = [
+    "-s"
+    "-X main.version=${finalAttrs.version}"
+  ];
 
   # some tests require network access
   doCheck = false;
 
   meta = {
     description = "Database client every command line junkie deserves";
+    longDescription = ''
+      Fast and lightweight interactive terminal-based UI application
+      for PostgreSQL, MySQL, SQLite, Oracle, and SQL Server.
+    '';
     homepage = "https://github.com/danvergara/dblab";
+    changelog = "https://github.com/danvergara/dblab/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ iamanaws ];
+    mainProgram = "dblab";
   };
 })
