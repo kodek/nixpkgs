9f74d7db0f6afed3837d20e8eb6e9803d02c6c72
DashieTM <fabio.lenherr@gmail.com>
2026-05-17 12:08:36 +0200

    imager: use gnu17 for build

--- pkgs/by-name/im/imager/package.nix
+++ pkgs/by-name/im/imager/package.nix
@@ -65,6 +65,7 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-unused-command-line-argument";
+  env.CFLAGS = "-std=gnu17";
 
   # Workaround for https://github.com/NixOS/nixpkgs/issues/304528
   env.GAG_CPP = if stdenv.hostPlatform.isDarwin then "${gfortran.outPath}/bin/cpp" else "cpp";
